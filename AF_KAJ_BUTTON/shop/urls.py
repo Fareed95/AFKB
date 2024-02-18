563daecd8f83a87fe9299d8c26fdaa2e05a5a75b
Fareed <fareedsayed95@gmail.com>
2024-02-18 17:46:37 +0530

    basic setup is ready

---
+++ AF_KAJ_BUTTON/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path("",views.index,name='index'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
