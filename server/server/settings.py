26cfb09834fdfa5dafd20f30d393e598a4ccffde
Fareed95 <fareedsayed95@gmail.com>
2024-09-20 18:15:33 +0530

    full setup with dockerfile is ready

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b!%u-9g)sr@3le_-o(59@&$_k(=yg11ry*h-#5$@)qae*r$-%m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
 
 
 # Application definition
