75dafe91edb981d4f92b8f662f429e7279035244
Fareed Khalil Sayed <143815597+Fareed95@users.noreply.github.com>
2024-10-01 23:37:38 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
         return;
       }
 
-      const response = await fetch('http://127.0.0.1:8000/api/user', {
+      const response = await fetch('http://54.211.219.118:8000/api/user', {
         method: 'GET',
         headers: {
           Authorization: `${token}`,
