63dd90bc17ff0e1a946808728bcfc99659ae437e
Fareed95 <fareedsayed95@gmail.com>
2024-10-02 12:24:23 +0530

    updated the docker file

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,9 +3,12 @@
 # Wait for the database to be ready (optional, adjust as needed)
 # For example, using a wait-for-it script or similar is common
 # ./wait-for-it.sh db:5432 --
+echo "Installing WeasyPrint dependencies..."
+sudo apt install libpango-1.0-0 libgdk-pixbuf2.0-dev libffi-dev libcairo2 -y
+
 
 # Run migrations
-python manage.py migrate
+# python manage.py migrate
 
 # Collect static files (optional, if you are using Django's static files)
 python manage.py collectstatic --noinput
